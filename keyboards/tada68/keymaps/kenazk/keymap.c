fccfa317ead9d4ec2e7d8aaaaa93461d7d32fdf6
Kenaz Kwa <kenaz@puppet.com>
2018-07-02 16:19:14 -0700

    Adding tap dance to ESC

--- keyboards/tada68/keymaps/kenazk/keymap.c
+++ keyboards/tada68/keymaps/kenazk/keymap.c
@@ -11,13 +11,13 @@
 
 //Tap Dance Declarations
 enum {
-  TD_HYPR_CAPS = 0
+  TD_GRV_ESC = 0
 };
 
 //Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-  //Tap once for Hyper, twice for Caps Lock
-  [TD_HYPR_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_HYPR, KC_CAPS)
+  //Tap once for GRV, twice for ESC
+  [TD_GRV_ESC]  = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_ESC)
 // Other declarations would go here, separated by commas, if you have them
 };
 
@@ -32,15 +32,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
    * |----------------------------------------------------------------|
-   * |Ctrl|Alt |Cmd |        Space          |Cmd|Alt|Fn | Lef|Dow|Rig |
+   * |Ctrl|Alt |Cmd |        Space          |Cmd|FN|Alt | Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
 [_BL] = LAYOUT_ansi(
-  KC_GESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
+  TD(TD_GRV_ESC),  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_GRV, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_DEL, \
-  TD(TD_HYPR_CAPS), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_PGUP,  \
+  KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_PGUP,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,KC_UP,KC_PGDN, \
-  KC_LCTL, KC_LALT,KC_LGUI,                KC_SPC,                         KC_RGUI,KC_RALT,MO(_FL), KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_LCTL, KC_LALT,KC_LGUI,                KC_SPC,                         KC_RGUI,MO(_FL),KC_RALT, KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
    * ,----------------------------------------------------------------.
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |      |<- |Dn | ->|   |   |   |   |   |   |   |   |        |End |
    * |----------------------------------------------------------------|
-   * |        |   |   |Bl-|BL |BL+|   |    | VU-|VU+|MUT| McL|MsU|McR |
+   * |        |   |   |Bl-|BL |BL+|   | VU-|VU+|MUT|   |  McL|MsU|McR |
    * |----------------------------------------------------------------|
    * |    |    |    |                       |   |   |    |MsL|MsD|MsR |
    * `----------------------------------------------------------------'
@@ -59,6 +59,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILDE,KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS ,  \
   _______,_______, KC_UP,_______,_______, _______,_______,_______,_______,_______,_______,_______,_______, _______,KC_HOME, \
   _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,_______,_______,_______,_______,_______,_______,        _______,KC_END, \
-  _______,_______,_______,BL_TOGG, BL_DEC,BL_INC, _______,_______,KC_MUTE,KC_VOLD,KC_VOLU,KC_BTN1, KC_MS_U, KC_BTN2, \
+  _______,_______,_______,BL_TOGG, BL_DEC,BL_INC, _______,KC_MUTE,KC_VOLD,KC_VOLU,_______,KC_BTN1, KC_MS_U, KC_BTN2, \
   _______,_______,_______,                 _______,               _______,_______,_______,KC_MS_L,KC_MS_D, KC_MS_R),
 };
